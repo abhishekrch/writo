63a2e358aca6ab4528737cb4e203987584b451eb
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-03-24 23:43:21 +0530

    wip(articles): implement articles overview page

---
+++ app/dashboard/sites/[siteid]/page.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { Book, PlusCircle, Settings } from "lucide-react";
+import Link from "next/link";
+
+export default function SiteIdRoute() {
+  return (
+    <>
+      <div className="flex w-full justify-end gap-x-4">
+        <Button asChild variant="secondary">
+          <Link href="#">
+            <Book className="size-4 mr-2" />
+            View Blog
+          </Link>
+        </Button>
+        <Button asChild variant="secondary">
+          <Link href="#">
+            <Settings className="size-4 mr-2" />
+            Settings
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href="#">
+            <PlusCircle className="size-4 mr-2" />
+            Create Article
+          </Link>
+        </Button>
+      </div>
+    </>
+  );
+}

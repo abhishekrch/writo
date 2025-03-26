cfbe282bc06e8c57c9e84f1809b33d678f026ce6
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-03-26 23:36:11 +0530

    feat(article): articles overview

--- app/dashboard/sites/[siteid]/page.tsx
+++ app/dashboard/sites/[siteid]/page.tsx
@@ -1,8 +1,43 @@
+import prisma from "@/app/utils/db";
 import { Button } from "@/components/ui/button";
-import { Book, PlusCircle, Settings } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Book, FileIcon, PlusCircle, Settings } from "lucide-react";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function SiteIdRoute() {
+async function getData(userId: string, siteId: string) {
+  const data = await prisma.post.findMany({
+    where: {
+      userId: userId,
+      siteId: siteId,
+    },
+    select: {
+      image: true,
+      title: true,
+      createdAt: true,
+      id: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return data;
+}
+
+export default async function SiteIdRoute({
+  params,
+}: {
+  params: { siteId: string };
+}) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
+
+  const data = await getData(user.id, params.siteId);
+
   return (
     <>
       <div className="flex w-full justify-end gap-x-4">
@@ -25,6 +60,31 @@ export default function SiteIdRoute() {
           </Link>
         </Button>
       </div>
+      {data === undefined || data.length === 0 ? (
+        <div
+          className="flex flex-col items-center justify-center rounded-md border 
+      border-dashed p-8 text-center animate-in fade-in-50"
+        >
+          <div className="flex size-20 items-center justify-center rounded-full bg-primary/10">
+            <FileIcon className="size-10 text-primary" />
+          </div>
+          <h2 className="mt-6 text-xl font-semibold">
+            You don't have any sites yet.
+          </h2>
+          <p className="mb-8 mt-2 text-center text-sm leading-tight text-muted-foreground max-w-sm mx-auto">
+            You currently don't have any sites. Please create some so that you
+            can see them here
+          </p>
+          <Button asChild>
+            <Link href={"/dashboard/sites/new"}>
+              <PlusCircle className="mr-2 size-4" />
+              Create Site
+            </Link>
+          </Button>
+        </div>
+      ) : (
+        <h1>not empty</h1>
+      )}
     </>
   );
 }

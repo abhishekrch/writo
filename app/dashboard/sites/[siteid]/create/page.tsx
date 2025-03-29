cd6cbc54158f8685131482f6b350c80fa49e389a
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-03-29 17:45:26 +0530

    feat(upload): integrate UploadThing for image uploads

--- app/dashboard/sites/[siteid]/create/page.tsx
+++ app/dashboard/sites/[siteid]/create/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { UploadDropzone } from "@/app/utils/UploadthingComponents";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,7 +11,8 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ArrowLeft } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { ArrowLeft, Atom } from "lucide-react";
 import Link from "next/link";
 
 export default function ArticleCreationRoute({
@@ -31,7 +35,45 @@ export default function ArticleCreationRoute({
           <CardTitle>Article Details</CardTitle>
           <CardDescription>This is the details of the article.</CardDescription>
         </CardHeader>
-      
+        <CardContent>
+          <form className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label>Title</Label>
+              <Input placeholder="Nextjs blogging application"></Input>
+            </div>
+
+            <div className="grid gap-2">
+              <Label>Slug</Label>
+              <Input placeholder="Article Slug"></Input>
+              <Button className="w-fit" variant="secondary" type="button">
+                <Atom className="size-4 mr-2" /> Generate Slug
+              </Button>
+            </div>
+
+            <div className="grid gap-2">
+              <Label>Small Description</Label>
+              <Textarea
+                placeholder="Small Description for your blog site"
+                className="h-32"
+              ></Textarea>
+            </div>
+
+            <div className="grid gap-2">
+              <Label> Cover Image </Label>
+              <UploadDropzone
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                  console.log("Files: ", res);
+                  alert("Upload completed successfully!");
+                }}
+                onUploadError={(error) => {
+                  console.error("Error: ", error);
+                  alert(`Error: ${error.message}`);
+                }}
+              />
+            </div>
+          </form>
+        </CardContent>
       </Card>
     </>
   );

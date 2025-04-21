9972928517130df5d6c2271d58bec2e6d2624463
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-04-21 22:05:43 +0530

    feat(pricing): create pricing table

--- app/components/shared/Pricing.tsx
+++ app/components/shared/Pricing.tsx
@@ -1,3 +1,49 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Check } from "lucide-react";
+
+interface iAppProps {
+  id: number;
+  cardTitle: string;
+  cardDescription: string;
+  priceTitle: string;
+  benefits: string[];
+}
+
+export const PricingPlans: iAppProps[] = [
+  {
+    id: 0,
+    cardTitle: "Freelancer",
+    cardDescription: "The best pricing plan for people starting out.",
+    benefits: [
+      "1 Site",
+      "Up to 1000 Visitors",
+      "Up to 1000 Visitors",
+      "Up to 1000 Visitors",
+    ],
+    priceTitle: "Free",
+  },
+  {
+    id: 1,
+    cardTitle: "Startup",
+    cardDescription: "The best pricing plan for professionals.",
+    priceTitle: "$29",
+    benefits: [
+      "Unlimited Sites",
+      "Unimlited Visitors",
+      "Unimlited Visitors",
+      "Unimlited Visitors",
+    ],
+  },
+];
+
 export function PricingTable() {
   return (
     <>
@@ -9,8 +55,48 @@ export function PricingTable() {
       </div>
 
       <p className="mx-auto mt-6 max-w-2xl text-center leading-tight text-muted-foreground">
-      Get your words out there with a beautiful, minimal blog. No frills, no fees—just focus on writing.
+        Get your words out there with a beautiful, minimal blog. No frills, no
+        fees—just focus on writing.
       </p>
+
+      <div className="grid grid-cols-1 gap-8 mt-5 lg:grid-cols-2">
+        {PricingPlans.map((item) => (
+          <Card key={item.id} className={item.id === 1 ? "border-primary" : ""}>
+            <CardHeader>
+              <CardTitle>
+                {item.id === 1 ? (
+                  <div className="flex items-center justify-between">
+                    <h3 className="text-primary">Startup</h3>
+
+                    <p className="rounded-full bg-primary/20 px-3 py-1 text-xs font-semibold leading-5 text-primary">
+                      Most popular
+                    </p>
+                  </div>
+                ) : (
+                  <>{item.cardTitle}</>
+                )}
+              </CardTitle>
+              <CardDescription>{item.cardDescription}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p className="mt-6 text-4xl font-bold tracking-tight">
+                {item.priceTitle}
+              </p>
+              <ul className="mt-8 space-y-3 text-sm leading-6 text-muted-foreground">
+                {item.benefits.map((benefit, index) => (
+                  <li key={index} className="flex gap-x-3">
+                    <Check className="text-primary size-5" />
+                    {benefit}
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+            <CardFooter>
+              <Button>Choose Plan</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </>
   );
 }

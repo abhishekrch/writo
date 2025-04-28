38182d4f5399835b449e67aa0ebc9ceff73ab1b6
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-04-28 23:10:11 +0530

    feat(pricing): add view subscriptions

--- app/dashboard/pricing/page.tsx
+++ app/dashboard/pricing/page.tsx
@@ -1,9 +1,58 @@
+import { SubmitButton } from "@/app/components/dashboard/SubmitButtons";
 import { PricingTable } from "@/app/components/shared/Pricing";
+import prisma from "@/app/utils/db";
+import { requireUser } from "@/app/utils/requireUser";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+async function getData(userId: string) {
+  const data = await prisma.subscription.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      status: true,
+      User: {
+        select: {
+          customerId: true,
+        },
+      },
+    },
+  });
 
-export default function PricingPage() {
-    return (
-        <div>
-            <PricingTable />
-        </div>
-    )
+  return data;
+}
+
+export default async function PricingPage() {
+    const user = await requireUser();
+    const data = await getData(user.id);
+
+    if (data?.status === "active") {
+        return (
+          <Card className="w-full ">
+            <CardHeader>
+              <CardTitle>Edit Subscription</CardTitle>
+              <CardDescription>
+                Click on the button below, this will give you the opportunity to
+                change your payment details and view your statement at the same
+                time...
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <SubmitButton text="View Subscription Details" />
+              </form>
+            </CardContent>
+          </Card>
+        );
+      }
+  return (
+    <div>
+      <PricingTable />
+    </div>
+  );
 }
